490454d4d92ef51d37074757f27b8fc3063f1ef4
Maarten A. Breddels <breddels@astro.rug.nl>
2017-09-05 18:43:58 +0200

    added count2 statistic, trying optimizing it

--- benchmark.py
+++ benchmark.py
@@ -1,6 +1,6 @@
+import statx
 import vaex
 import numpy as np
-import statx
 import concurrent.futures
 import multiprocessing
 import threading
@@ -13,16 +13,19 @@ thread_pool = concurrent.futures.ThreadPoolExecutor(max_workers=thread_count)
 print("threads", thread_count)
 
 
-ds = vaex.open("/Users/maartenbreddels/vaex/data/gaia/gaia-dr1-10percent.hdf5")
+ds = vaex.open("gaia-dr1-10percent.hdf5")
 x = ds.data.ra
 binby = 'ra'
 
 y = ds.data.dec
 expr = 'dec'
 
-limits = [0, 360]
-shape = 12
+binby2 = ['ra', 'dec']
 
+limits = [0, 360]
+limits2 = [[0, 360], [-180, 180]]
+shape = 12
+decimal = 1
 chuck_size = 1024*1024
 N_chunks = math.ceil(len(x) / chuck_size)
 def chop(ar):
@@ -31,7 +34,7 @@ chunks_x = chop(x)
 chunks_y = chop(y)
 
 print(len(x), np.sum(len(k) for k in chunks_x))
-np.testing.assert_almost_equal(np.sum(x), np.sum(np.sum(k) for k in chunks_x), decimal=3)
+np.testing.assert_almost_equal(np.sum(x), np.sum(np.sum(k) for k in chunks_x), decimal=decimal)
 
 
 def vaex_1():
@@ -74,37 +77,74 @@ def statx_2():
     return res
 
 
+
+def vaex_3():
+    return ds.count(binby=binby2, limits=limits2, shape=shape)
+
+def statx_3():
+    grids = np.zeros((thread_count, shape, shape), dtype=np.float64)
+    lock = threading.Lock()
+    thread_indices = iter(range(100))
+    local = threading.local()
+    def do_work(chunk_index):
+        with lock:
+            if not hasattr(local, 'index'):
+                local.index = next(thread_indices)
+        statx._count2(grids[local.index], chunks_x[chunk_index], *limits2[0], chunks_y[chunk_index], *limits2[1])
+    # map
+    __ = list(thread_pool.map(do_work, range(len(chunks_x))))
+    # reduce
+    res = np.sum(grids, axis=0)
+    return res
+
+
 if __name__ == "__main__":
+    res_vaex_3 = vaex_3()
+    res_statx_3 = statx_3()
+    np.testing.assert_almost_equal(res_vaex_3, res_statx_3, decimal=decimal)
+
+
+
     res_vaex_2 = vaex_2()
     res_statx_2 = statx_2()
-    np.testing.assert_almost_equal(res_vaex_2, res_statx_2, decimal=4)
+    np.testing.assert_almost_equal(res_vaex_2, res_statx_2, decimal=decimal)
 
     res_vaex_1 = vaex_1()
     res_statx_1 = statx_1()
-    np.testing.assert_almost_equal(res_vaex_1, res_statx_1)
+    np.testing.assert_almost_equal(res_vaex_1, res_statx_1, decimal=decimal)
 
 
     N = 3
+    print("job 1 (count2d)")
+    t_total = timeit.timeit('statx_3()', setup='from __main__ import statx_3', number=N)
+    t = t_total / N
+    print("spectabular:\t%5.3f seconds" % t)
 
-    print("job 1 (count)")
+
+    t_total = timeit.timeit('vaex_3()',  setup='from __main__ import vaex_3', number=N)
+    t = t_total / N
+    print("vaex:\t\t%5.3f seconds" % t)
+
+
+    print("job 2 (count)")
     t_total = timeit.timeit('statx_1()', setup='from __main__ import statx_1', number=N)
     t = t_total / N
-    print("statx: %f seconds" % t)
+    print("spectabular:\t%5.3f seconds" % t)
 
 
     t_total = timeit.timeit('vaex_1()',  setup='from __main__ import vaex_1', number=N)
     t = t_total / N
-    print("vaex: %f seconds" % t)
+    print("vaex:\t\t%5.3f seconds" % t)
 
 
-    print("job 2 (sum)")
+    print("job 3 (sum)")
     t_total = timeit.timeit('statx_2()', setup='from __main__ import statx_2', number=N)
     t = t_total / N
-    print("statx: %f seconds" % t)
+    print("spectabular:\t%5.3f seconds" % t)
 
 
     t_total = timeit.timeit('vaex_2()',  setup='from __main__ import vaex_2', number=N)
     t = t_total / N
-    print("vaex: %f seconds" % t)
+    print("vaex:\t\t%5.3f seconds" % t)
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #include "xtensor/xmath.hpp"
 #include "xtensor/xarray.hpp"
+#include "xtensor/xview.hpp"
 
 #define FORCE_IMPORT_ARRAY
 #include "xtensor-python/pyarray.hpp"
@@ -16,26 +17,83 @@ namespace py = pybind11;
 
 inline double sum(xt::pyarray<double> &m)
 {
-    py::gil_scoped_release release;
+    //py::gil_scoped_release release;
     return std::accumulate(m.begin(), m.end(), 0.0);
 }
 
+template<typename E_GRID, typename X_TYPE>
+//inline void _countX(xt::xexpression<E_GRID>& grid_expression, const int i, const xt::pytensor<X_TYPE,1> &x, const double xmin, const double xmax)
+inline void  __attribute__((always_inline)) _countX(E_GRID& grid, const int i, const xt::pytensor<X_TYPE,1> &x, const double& xmin, const double& xmax, const int& shape0)
+{
+    //E_GRID& grid = grid_expression.derived_cast();
+    //const double scale = 1/(xmax - xmin);
+    //const double vmin = xmin;
+    //const int shape0 = grid.shape()[0];
+    const double x_value = x[i];
+    const double scaled = (x_value - xmin) * xmax;
+    if( (scaled >= 0) & (scaled < 1) ) {
+        int index = (int)(scaled * shape0);
+        grid(index) += 1;
+    }
+}
+
+
+template<typename E_GRID, typename X_TYPE, typename...TAIL>
+//inline void _countX(xt::xexpression<E_GRID>& grid_expression, const int i, const xt::pytensor<X_TYPE,1> &x, const double xmin, const double xmax, TAIL... tail)
+Cinline void  __attribute__((always_inline)) _countX(E_GRID& grid, const int i, const xt::pytensor<X_TYPE,1> &x, const double& xmin, const double& xmax, const int& shape0, TAIL... tail)
+{
+    //E_GRID& grid = grid_expression.derived_cast();
+    //const double scale = 1/(xmax - xmin);
+    //const double vmin = xmin;
+    //const int shape0 = grid.shape()[0];
+    const double x_value = x[i];
+    const double scaled = (x_value - xmin) * xmax;
+    if( (scaled >= 0) & (scaled < 1) ) {
+        const int index = (int)(scaled * shape0);
+        auto&& subgrid = xt::view(grid, index);
+        _countX(subgrid, i, tail...);
+    }
+}
+
+inline void _count2(xt::pytensor<double,2> &grid, const xt::pytensor<double,1> &x, const double xmin, const double xmax, const xt::pytensor<double,1> &y, const double ymin, const double ymax)
+{
+    py::gil_scoped_release release;
+    const int length = x.shape()[0];
+    const double scaley = 1/(ymax-ymin);
+    const double scalex = 1/(xmax-xmin);
+    const int shapex = grid.shape()[0];
+    const int shapey = grid.shape()[1];
+    //*
+    for(int i = 0 ; i < length; i++)
+        _countX(grid, i, x, xmin, scalex, shapex, y, ymin, scaley, shapey);
+    /*/
+    const int shapex = grid.shape()[0];
+    const int shapey = grid.shape()[1];
+    for(int i = 0 ; i < length; i++) {
+        const double x_value = x[i];
+        const double scaledx = (x_value - xmin) * scalex;
+        if( (scaledx >= 0) & (scaledx < 1) ) {
+            int indexx = (int)(scaledx * shapex);
+            const double y_value = y[i];
+            const double scaledy = (y_value - ymin) * scaley;
+            auto subgrid = xt::view(grid, indexx);
+            if( (scaledy >= 0) & (scaledy < 1) ) {
+                int indexy = (int)(scaledy * shapey);
+                subgrid(indexy) += 1;
+            }
+        }
+    }
+
+   /**/
+}
 
 inline void _count(xt::pytensor<double,1> &grid, xt::pytensor<double,1> &x, double xmin, double xmax)
 {
     py::gil_scoped_release release;
-    double scale = 1/(xmax - xmin);
-    double vmin = xmin;
-    const int shape0 = grid.shape()[0];
-    for (const double& x_value : x) {
-        double scaled = (x_value - vmin) * scale;
-        if( (scaled >= 0) & (scaled < 1) ) {
-            int index = (int)(scaled * shape0);
-            grid(index) += 1;
-        }
-    }
+    int length = x.shape()[0];
+    for(int i = 0 ; i < length; i++)
+        _countX(grid, i, x, xmin, 1/(xmax-xmin), grid.shape()[0]);
 }
-
 
 inline xt::pyarray<double> count(xt::pytensor<double,1> &x, double xmin, double xmax, int shape)
 {
@@ -65,6 +123,7 @@ inline void _sum(xt::pytensor<double,1> &grid, xt::pytensor<double,1> &values, x
 }
 
 
+
 PYBIND11_PLUGIN(statx)
 {
     xt::import_numpy();
@@ -85,6 +144,7 @@ PYBIND11_PLUGIN(statx)
 
     //def("count", count, "counts values on a regular N-d grid (multithreaded)");
     m.def("_count", _count, "implementation of count1d, but single threaded, and does not allocate a grid");
+    m.def("_count2", _count2, "implementation of count2d, but single threaded, and does not allocate a grid");
     //m.def("sum", sum, "calculates sum values on a regular N-d grid (multithreaded)");
     m.def("_sum", _sum, "implementation of sum , but single threaded, and does not allocate a grid");
     return m.ptr();
